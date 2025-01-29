7a57eceafe467da3025159b4458c80bb02260354
Luka Banovic <luka.banovic@gmail.com>
2025-01-29 10:31:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://yourusername.github.io',
+  site: 'https://IRNAS.github.io',
   base: '/lightbrite-eq',
 });
